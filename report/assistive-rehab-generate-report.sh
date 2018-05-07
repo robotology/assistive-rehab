5b32e33cb516e9281f5ba575a71001c98669d153
Valentina Vasco <valentina.vasco@iit.it>
2018-05-07 14:26:55 +0200

    updated report dir

--- report/assistive-rehab-generate-report.sh
+++ report/assistive-rehab-generate-report.sh
@@ -3,13 +3,14 @@
 #This script converts ipynb notebook file into html
 
 FILENAME=$(yarp resource --context AssistiveRehab --from report.ipynb | awk '{gsub(/\"/,"")};1')
+CURR_DIR=$PWD
 
 #Convert to notebook
 echo "Converting $FILENAME to notebook..."
-eval "jupyter nbconvert --execute --inplace $FILENAME"
+eval "jupyter nbconvert --execute --output-dir $CURR_DIR --inplace $FILENAME"
 
 #Convert to html
 echo "Converting $FILENAME to html..."
-eval "jupyter nbconvert --to html $FILENAME"
+eval "jupyter nbconvert --output-dir $CURR_DIR --to html $FILENAME"
 
 echo "Done"
