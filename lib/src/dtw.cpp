1cb2cb6a300b2541037e475eb0505b508771f0f2
Valentina Vasco <Valentina.Vasco@iit.it>
2018-07-06 15:00:37 +0200

    [lib] added dtw and test (#81)
    * [lib] added DTW + relative test

---
+++ lib/src/dtw.cpp
@@ -0,0 +1,163 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright (C) 2018 Fondazione Istituto Italiano di Tecnologia (IIT)        *
+ * All Rights Reserved.                                                       *
+ *                                                                            *
+ ******************************************************************************/
+
+/**
+ * @file dtw.cpp
+ * @authors: Valentina Vasco <valentina.vasco@iit.it>
+ */
+
+#include <iostream>
+#include <yarp/sig/all.h>
+#include <yarp/math/Math.h>
+#include "AssistiveRehab/dtw.h"
+
+using namespace std;
+using namespace yarp::sig;
+
+Dtw::Dtw() : win(-1)
+{
+    d = 0.0;
+}
+
+Dtw::Dtw (const int &win_)
+{
+    win = win_;
+    d = 0.0;
+}
+
+Matrix Dtw::initialize(const int ns, const int nt)
+{
+    Matrix distMat(ns+1,nt+1);
+
+    //initialize distance matrix
+    for(int i=0;i<ns+1;i++)
+    {
+        for(int j=0;j<nt+1;j++)
+        {
+            distMat[i][j]=-1;
+        }
+    }
+    distMat[0][0]=0;
+    return distMat;
+}
+
+double Dtw::computeDistance(const vector<double> &s, const vector<double> &t, Matrix &distMat)
+{
+    //compute distance matrix
+    int ns=s.size();
+    int nt=t.size();
+    int j1,j2;
+    double cost,temp;
+    for(int i=1;i<=ns;i++)
+    {
+        if(win==-1)
+        {
+            j1=1;
+            j2=nt;
+        }
+        else
+        {
+            j1= i-win>1 ? i-win : 1;
+            j2= i+win<nt ? i+win : nt;
+        }
+        for(int j=j1;j<=j2;j++)
+        {
+            cost=sqrt((s[i-1]-t[j-1])*(s[i-1]-t[j-1]));
+            temp=distMat[i-1][j];
+            if(distMat[i][j-1]!=-1)
+            {
+                if(temp==-1 || distMat[i][j-1]<temp)
+                    temp=distMat[i][j-1];
+            }
+            if(distMat[i-1][j-1]!=-1)
+            {
+                if(temp==-1 || distMat[i-1][j-1]<temp)
+                    temp=distMat[i-1][j-1];
+            }
+            distMat[i][j]=cost+temp;
+        }
+    }
+
+    return(distMat[ns][nt]/nt);
+}
+
+int Dtw::getMin(Matrix &distMat, const int row, const int nt)
+{
+    int col=1;
+    double mind=distMat[row][col];
+    for(int j=1;j<nt+1;j++)
+    {
+        if(distMat[row][j]<mind)
+        {
+            mind=distMat[row][j];
+            col=j;
+        }
+    }
+    return col;
+}
+
+/***************************/
+/*  Mono-dimensional DTW   */
+/***************************/
+vector<double> Dtw::align(const vector<double> &s, const vector<double> &t)
+{
+    int ns=s.size();
+    int nt=t.size();
+
+    //create and initialize distance matrix
+    Matrix distMat = initialize(ns,nt);
+
+    //compute distance matrix
+    d=computeDistance(s,t,distMat);
+
+    //align
+    vector<double> res;
+    res.resize(ns);
+    for(int i=1;i<ns+1;i++)
+    {
+        int j=getMin(distMat,i,nt);
+        res[i-1]=t[j-1];
+    }
+
+    return res;
+}
+
+/***************************/
+/*  Multi-dimensional DTW  */
+/***************************/
+vector<vector<double>> Dtw::align(const vector<vector<double>> &s, const vector<vector<double>> &t)
+{
+    int n=s.size();
+    int ns=s[0].size();
+    int nt=t[0].size();
+    vector<vector<double>> res(n,vector<double>(nt));
+
+    //compute distance matrix for each component of the vectors
+    //vectors must have the same number of components
+    for(int l=0; l<n; l++)
+    {
+        //create and initialize distance matrix
+        Matrix distMat = initialize(ns,nt);
+
+        //compute distance
+        d+=computeDistance(s[l],t[l],distMat);
+
+        //align
+        for(int k=1;k<ns+1;k++)
+        {
+            int j=getMin(distMat,k,nt);
+            res[l][k-1]=t[l][j-1];
+        }
+    }
+
+    return res;
+}
+
+Dtw::~Dtw()
+{
+
+}
---
+++ test/test-dtwlib.cpp
@@ -0,0 +1,146 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright (C) 2018 Fondazione Istituto Italiano di Tecnologia (IIT)        *
+ * All Rights Reserved.                                                       *
+ *                                                                            *
+ ******************************************************************************/
+
+/**
+ * @file test-dtwlib.cpp
+ * @authors: Valentina Vasco <valentina.vasco@iit.it>
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include "AssistiveRehab/dtw.h"
+
+using namespace std;
+
+int main()
+{
+    ofstream outfile;
+    outfile.open("dtwlib-test.txt");
+
+    /*****************************/
+    /*    Different frequency    */
+    /*****************************/
+    cout << "### Generating 2 sinusoidal 1D vectors at different frequencies ###" << endl;
+    int n1=100, n2=100;
+    double f1=0.1, f2=0.8;
+    double A1=1.0, A2=1.0;
+    vector<double> v1_1(n1);
+    vector<double> v2_1(n2);
+
+    cout << "f1 = " << f1 << " f2 = " << f2 << endl;
+
+    double t=0.0;
+    for(int i=0; i<n1; i++)
+    {
+        v1_1[i]=A1*sin(2.0*M_PI*f1*t);
+//        outfile << v1[i] << " ";
+        t+=0.1;
+    }
+//    outfile << "\n";
+
+    t=0.0;
+    for(int i=0; i<n2; i++)
+    {
+        v2_1[i]=A2*sin(2.0*M_PI*f2*t);
+//        outfile << v2[i] << " ";
+        t+=0.1;
+    }
+//    outfile << "\n";
+
+    cout << "### Aligning vectors ###" << endl;
+    Dtw dtw1(-1);
+    vector<double> v3_1 = dtw1.align(v1_1,v2_1);
+
+    double d1 = dtw1.getDistance();
+    cout << "DTW distance = " << d1 << endl;
+    cout << endl;
+
+    /*****************************/
+    /*     Close frequencies     */
+    /*****************************/
+    cout << "### Generating 2 sinusoidal 1D vectors at close frequencies ###" << endl;
+    f1=0.7, f2=0.8;
+    vector<double> v1_2(n1);
+    vector<double> v2_2(n2);
+
+    cout << "f1= " << f1 << " f2 = " << f2 << endl;
+
+    t=0.0;
+//    cout << "v1: ";
+    for(int i=0; i<n1; i++)
+    {
+        v1_2[i]=A1*sin(2.0*M_PI*f1*t);
+//        outfile << v1[i] << " ";
+        t+=0.1;
+    }
+//    outfile << "\n";
+
+    t=0.0;
+    for(int i=0; i<n2; i++)
+    {
+        v2_2[i]=A2*sin(2.0*M_PI*f2*t);
+//        outfile << v2[i] << " ";
+        t+=0.1;
+    }
+//    outfile << "\n";
+
+    cout << "### Aligning vectors ###" << endl;
+    Dtw dtw2(-1);
+    vector<double> v3_2;
+    v3_2 = dtw2.align(v1_2,v2_2);
+
+    double d2 = dtw2.getDistance();
+    cout << "DTW distance = " << d2 << endl;
+    cout << endl;
+
+    cout << "### Generating 2 sinusoidal n-D vectors ###" << endl;
+
+    int n=6;
+    cout << "f1 = " << f1 << " f2 = " << f2 << " n = " << n << endl;
+    vector<vector<double>> v1_3d(n,vector<double>(n1));
+    vector<vector<double>> v2_3d(n,vector<double>(n2));
+
+    t=0.0;
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n1; j++)
+        {
+            v1_3d[i][j]=A1*sin(2.0*M_PI*f1*t);
+            outfile << v1_3d[i][j] << " ";
+            t+=0.1;
+        }
+        outfile << "\n";
+    }
+    outfile << "\n";
+
+    t=0.0;
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n2; j++)
+        {
+            v2_3d[i][j]=A2*sin(2.0*M_PI*f2*t);
+            outfile << v2_3d[i][j] << " ";
+            t+=0.1;
+        }
+        outfile << "\n";
+    }
+    outfile << "\n";
+
+    cout << "### Aligning vectors ###" << endl;
+    Dtw dtw_3d(-1);
+    vector<vector<double>> v3_3d = dtw_3d.align(v1_3d,v2_3d);
+    double d_3d = dtw_3d.getDistance();
+    cout << "DTW distance = " << d_3d << endl;
+    cout << endl;
+
+    outfile.close();
+
+    return EXIT_SUCCESS;
+}
