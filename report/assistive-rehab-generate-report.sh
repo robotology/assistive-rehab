95b95a6972b29512aa164a1e2c6273bca7dc4c06
Valentina Vasco <valentina.vasco@iit.it>
2018-12-14 14:45:24 +0100

    added english report

--- report/assistive-rehab-generate-report.sh
+++ report/assistive-rehab-generate-report.sh
@@ -2,15 +2,22 @@
 
 #This script converts ipynb notebook file into html
 
-FILENAME=$(yarp resource --context AssistiveRehab --from report.ipynb | awk '{gsub(/\"/,"")};1')
+FILENAME_ITA=$(yarp resource --context AssistiveRehab --from report-ita.ipynb | awk '{gsub(/\"/,"")};1')
+FILENAME_ENG=$(yarp resource --context AssistiveRehab --from report-eng.ipynb | awk '{gsub(/\"/,"")};1')
 CURR_DIR=$PWD
 
 #Convert to notebook
-echo "Converting $FILENAME to notebook..."
-eval "jupyter nbconvert --execute --output-dir $CURR_DIR --inplace $FILENAME"
+echo "Converting $FILENAME_ITA to notebook..."
+eval "jupyter nbconvert --execute --output-dir $CURR_DIR --inplace $FILENAME_ITA"
+
+echo "Converting $FILENAME_ENG to notebook..."
+eval "jupyter nbconvert --execute --output-dir $CURR_DIR --inplace $FILENAME_ENG"
 
 #Convert to html
-echo "Converting $FILENAME to html..."
-eval "jupyter nbconvert --output-dir $CURR_DIR --to html $FILENAME"
+echo "Converting $FILENAME_ITA to html..."
+eval "jupyter nbconvert --output-dir $CURR_DIR --to html $FILENAME_ITA"
+
+echo "Converting $FILENAME_ENG to html..."
+eval "jupyter nbconvert --output-dir $CURR_DIR --to html $FILENAME_ENG"
 
 echo "Done"
