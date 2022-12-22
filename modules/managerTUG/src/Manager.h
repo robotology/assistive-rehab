42c6685737add800206ba0e3c92e2aa030efe906
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2022-12-22 15:59:04 +0000

    refactored manager.h

--- modules/managerTUG/src/Manager.h
+++ modules/managerTUG/src/Manager.h
@@ -1,3 +1,35 @@
+#include <yarp/os/RFModule.h>
+#include <yarp/os/ResourceFinder.h>
+#include <yarp/os/Network.h>
+#include <yarp/os/Log.h>
+#include <yarp/os/LogStream.h>
+#include <yarp/os/BufferedPort.h>
+#include <yarp/os/RpcClient.h>
+#include <yarp/os/PeriodicThread.h>
+#include <yarp/math/Math.h>
+#include <yarp/os/LogComponent.h>
+
+#include <condition_variable>
+
+#include <mutex>
+#include <cmath>
+
+#include <AssistiveRehab/skeleton.h>
+
+#include "src/managerTUG_IDL.h"
+
+namespace
+{
+    YARP_LOG_COMPONENT(MANAGERTUG, "managerTUG")
+}
+
+using namespace std;
+using namespace yarp::os;
+using namespace yarp::sig;
+using namespace yarp::math;
+using namespace assistive_rehab;
+
+
 class Manager : public yarp::os::RFModule, public managerTUG_IDL
 {
 private:
@@ -7,13 +39,13 @@ private:
     double period;
     double pointing_time,arm_thresh;
     bool detect_hand_up;
-    Vector starting_pose,
-    Vector pointing_home,pointing_start,pointing_finish;
+    yarp::sig::Vector starting_pose,
+    yarp::sig::Vector pointing_home,pointing_start,pointing_finish;
     bool simulation,lock;
-    Vector target_sim;
-    string human_state;
-    vector<string> laser_adverb;
-    vector<double> engage_distance,engage_azimuth;
+    yarp::sig::Vector target_sim;
+    std::string human_state;
+    std::vector<std::string> laser_adverb;
+    std::vector<double> engage_distance,engage_azimuth;
 
     const int ok=Vocab32::encode("ok");
     const int fail=Vocab32::encode("fail");
@@ -71,4 +103,82 @@ private:
     TriggerManager *trigger_manager;
     ObstacleManager *obstacle_manager;
 public:
+
+    Manager()
+
+    bool attach(RpcServer &source) override;
+    
+    bool load_speak(const string &context, const string &speak_file);
+
+    bool speak(Speech &s);
+    
+    string get_sentence(string &value, const vector<shared_ptr<SpeechParam>> &p) const;
+    
+    string get_animation();
+    
+    bool play_animation(const string &name);
+    
+    bool play_from_last();
+   
+    bool set_auto();
+
+    bool send_stop(const RpcClient &port);
+   
+    bool is_navigating();
+    
+    bool disengage();
+    
+    void resume_animation();
+    
+    bool go_to(const Vector &target, const bool &wait);
+
+    bool remove_locked();
+   
+    bool start() override;
+    
+    bool trigger() override;
+   
+    bool set_target(const double x, const double y, const double theta) override;
+
+    double get_measured_time() override;
+
+    string get_success_status() override;
+
+    bool has_finished() override;
+
+    bool stop() override;
+
+    bool configure(ResourceFinder &rf) override;
+
+    double getPeriod() override;
+
+    bool updateModule() override;
+
+    void follow(const string &follow_tag);
+
+    void encourage(const double &timeout);
+
+    void start_interaction();
+
+    bool start_collection();
+ 
+    void set_walking_speed(const Bottle &r);
+
+    bool is_active();
+
+    string which_part();
+
+    bool point(const Vector &target, const string &part, const bool wait);
+
+    bool opcRead(const string &t, Property &prop, const string &tval="");
+
+    bool hasLine(Property &prop);
+
+    bool getWorld(const Property &prop);
+
+    bool findLocked(string &t);
+
+    bool interruptModule() override;
+
+    bool close() override;
 };
